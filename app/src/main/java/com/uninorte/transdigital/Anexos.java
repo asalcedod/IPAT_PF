04d9e34021c18acdc96d555f7c3c82418f98a281
LauryV <lauryv@uninorte.edu.co>
2017-04-23 21:47:45 -0500

    DatePickerDialog and Anexos (Photos)

--- app/src/main/java/com/uninorte/transdigital/Anexos.java
+++ app/src/main/java/com/uninorte/transdigital/Anexos.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +14,8 @@ import android.widget.TextView;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
-public class Anexos extends Activity implements OnClickListener{
-    Button btnCam;
+public class Anexos extends AppCompatActivity implements OnClickListener{
+    FloatingActionButton btnCam;
     ImageView img;
     Bitmap bmp;
     Intent i;
@@ -22,11 +24,21 @@ public class Anexos extends Activity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.anexos);
-        init();
+
+        //Floating Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.camara);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                init();
+            }
+        });
+
     }
 
     private void init() {
-        btnCam = (Button) findViewById(R.id.cam);
+        btnCam = (FloatingActionButton) findViewById(R.id.camara);
         btnCam.setOnClickListener(this);
 
         img = (ImageView) findViewById(R.id.imagen);
@@ -37,16 +49,7 @@ public class Anexos extends Activity implements OnClickListener{
         return super.onCreateOptionsMenu(menu);
     }
 
-    public void ActCam(View view) {
-        int id;
-        id= view.getId();
-        switch(id){
-            case R.id.cam:
-                i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i,cons);
-                break;
-        }
-    }
+
 
 
     @Override
@@ -54,7 +57,7 @@ public class Anexos extends Activity implements OnClickListener{
         int id;
         id= v.getId();
         switch(id){
-            case R.id.cam:
+            case R.id.camara:
                 i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(i,cons);
                 break;
--- app/src/main/java/com/uninorte/transdigital/Cond_Vehi_Prop.java
+++ app/src/main/java/com/uninorte/transdigital/Cond_Vehi_Prop.java
@@ -1,17 +1,30 @@
 package com.uninorte.transdigital;
-
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TabHost;
 
-public class Cond_Vehi_Prop extends AppCompatActivity {
+public class Cond_Vehi_Prop extends AppCompatActivity implements View.OnClickListener{
     TabHost TbH;
+    private static final String TAG = "LogsAndroid";
+    Button bfechnacond,bfechalicen,bfechavencSoat,bfechavencSSC,bfechavencSSE;
+    EditText efechnacond,efechalicen,efechavencSoat,efechavencSSC,efechavencSSE;
+    private int dia,mes,ano;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cond__vehi__prop);
 
+        //tabs---------------------------------------------------------------------------------
         TbH = (TabHost) findViewById(R.id.tabHost); //llamamos al Tabhost
         TbH.setup(); //lo activamos
 
@@ -20,9 +33,7 @@ public class Cond_Vehi_Prop extends AppCompatActivity {
         TabHost.TabSpec tab2 = TbH.newTabSpec("tab2");
         TabHost.TabSpec tab3 = TbH.newTabSpec("tab3");
         TabHost.TabSpec tab4 = TbH.newTabSpec("tab4");
-
-
-
+        TabHost.TabSpec tab5 = TbH.newTabSpec("tab5");
 
         tab1.setIndicator("Datos Personales");    //qué queremos que aparezca en las pestañas
         tab1.setContent(R.id.datoscond); //definimos el id de cada Tab (pestaña)
@@ -30,23 +41,91 @@ public class Cond_Vehi_Prop extends AppCompatActivity {
         tab2.setIndicator("Detalles Conductores");
         tab2.setContent(R.id.condetalles);
 
-        tab1.setIndicator("Datos Vehiculo");
-        tab1.setContent(R.id.datosvehic);
+        tab3.setIndicator("Datos Vehiculo");
+        tab3.setContent(R.id.datosvehic);
 
-        tab3.setIndicator("Detalles Vehiculo");
-        tab3.setContent(R.id.vehicdetalles);
+        tab4.setIndicator("Detalles Vehiculo");
+        tab4.setContent(R.id.vehicdetalles);
 
-        tab4.setIndicator("Propietarios");
-        tab4.setContent(R.id.propiet);
+        tab5.setIndicator("Propietarios");
+        tab5.setContent(R.id.propiet);
 
         TbH.addTab(tab1); //añadimos los tabs ya programados
         TbH.addTab(tab2);
         TbH.addTab(tab3);
         TbH.addTab(tab4);
+        TbH.addTab(tab5);
+        //end tabs-----------------------------------------------------------------------------
+
+        //DATOS PERSONALES DEL CONDUCTOR
+        bfechnacond = (Button) findViewById(R.id.bfechnacond);
+        efechnacond = (EditText) findViewById(R.id.efechnacond);
+        bfechnacond.setOnClickListener(this);
+
+        //DETALLES DEL CONDUCTOR
+        bfechalicen = (Button) findViewById(R.id.bfechnacond);
+        efechalicen = (EditText) findViewById(R.id.efechnacond);
+        bfechalicen.setOnClickListener(this);
+
+        //VEHIC DETALLES
+        //fechas de vencimiento
+        bfechavencSoat = (Button) findViewById(R.id.bfechavencSoat);
+        efechavencSoat = (EditText) findViewById(R.id.efechnacond);
+        bfechavencSoat.setOnClickListener(this);
+
+        bfechavencSSC = (Button) findViewById(R.id.bfechavencSoat);
+        efechavencSSC= (EditText) findViewById(R.id.efechnacond);
+        bfechavencSSC.setOnClickListener(this);
+
+        bfechavencSSE = (Button) findViewById(R.id.bfechavencSoat);
+        efechavencSSE= (EditText) findViewById(R.id.efechnacond);
+        bfechavencSSE.setOnClickListener(this);
+    }
 
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bfechnacond:
+                fecha();
+                break;
+            case R.id.bfechalicen:
+                fecha();
+                break;
+            case R.id.bfechavencSoat:
+                fecha();
+                break;
+            case R.id.bfechavencSSC:
+                fecha();
+                break;
+            case R.id.bfechavencSSE:
+                fecha();
+                break;
+            default:
+                break;
+            }
+    }
 
+    @TargetApi(Build.VERSION_CODES.N)
+    public void fecha(){
+        final Calendar c;
+        c = Calendar.getInstance();
+        dia=c.get(Calendar.DAY_OF_MONTH);
+        mes=c.get(Calendar.MONTH);
+        ano=c.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                efechnacond.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+
+            }
+        }
+                ,dia,mes,ano);
+        datePickerDialog.show();
 
     }
+
 }
+

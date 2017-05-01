6f54305796fe6a8ef841689dbec043c35b1aa5c4
Antonio <antony9409@gmail.com>
2017-04-30 20:11:03 -0500

    Detallescond

--- app/src/main/java/com/uninorte/transdigital/DetallesConductores.java
+++ app/src/main/java/com/uninorte/transdigital/DetallesConductores.java
@@ -19,11 +19,11 @@ import java.util.List;
 
 public class DetallesConductores extends Fragment {
     ImageButton imageButtonSave;
-    EditText n,m,o,p,q;
+    EditText egez,nlic,fecha,cof,hosp,extra;
     RadioGroup rg,examen,aut,ebriagez,gradoE,sustancias,portalicencia,estadol,catego,co_of,chal,casc,cintu;
     public Spinner categoria,clasev,clases,mdt,radioa;
     public int dia,mes,ano;
-    public String cat="",g,e,a,embr,gE,sust,portlia,idlicencia,el,categ,cod_of,chaleco,casco,cinturon,hospital,descip;
+    public String cat="",g,e,a,embr,gE,sust,portlia,idlicencia,fcha,el,categ,cod_of,chaleco,casco,cinturon,hospital,descip;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -48,17 +48,6 @@ public class DetallesConductores extends Fragment {
                 cat="";
             }
         });
-
-
-        imageButtonSave = (ImageButton) rootView.findViewById(R.id.SaveDetaCond);
-        imageButtonSave.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
         rg=(RadioGroup) rootView.findViewById(R.id.grav);
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -108,6 +97,7 @@ public class DetallesConductores extends Fragment {
                 }
             }
         });
+        egez=(EditText) rootView.findViewById(R.id.embiaguezcond);
         sustancias=(RadioGroup) rootView.findViewById(R.id.sustanpsicoac);
         sustancias.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -132,6 +122,7 @@ public class DetallesConductores extends Fragment {
                 }
             }
         });
+        nlic=(EditText) rootView.findViewById(R.id.idLicenConductor);
         estadol=(RadioGroup) rootView.findViewById(R.id.estadoLicenCond);
         estadol.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -144,6 +135,8 @@ public class DetallesConductores extends Fragment {
                 }
             }
         });
+        fecha=(EditText) rootView.findViewById(R.id.efechalicen);
+        cof=(EditText)rootView.findViewById(R.id.codOfiTraCon);
         chal=(RadioGroup) rootView.findViewById(R.id.idChaleco);
         chal.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -162,9 +155,9 @@ public class DetallesConductores extends Fragment {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // TODO Auto-generated method stub
                 if (checkedId == R.id.Si) {
-                    chaleco= "Si";
+                    casco= "Si";
                 } else if (checkedId == R.id.No) {
-                    chaleco = "No";
+                    casco = "No";
                 }
             }
         });
@@ -174,12 +167,28 @@ public class DetallesConductores extends Fragment {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 // TODO Auto-generated method stub
                 if (checkedId == R.id.Si) {
-                    chaleco= "Si";
+                    cinturon= "Si";
                 } else if (checkedId == R.id.No) {
-                    chaleco = "No";
+                    cinturon = "No";
                 }
             }
         });
+        hosp=(EditText)rootView.findViewById(R.id.atencionCond);
+        extra=(EditText)rootView.findViewById(R.id.DescripcLesionCond);
+
+        imageButtonSave = (ImageButton) rootView.findViewById(R.id.SaveDetaCond);
+        imageButtonSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                gE=egez.getText().toString();
+                idlicencia=nlic.getText().toString();
+                fcha=fecha.getText().toString();
+                cod_of=cof.getText().toString();
+                hospital=hosp.getText().toString();
+                descip=extra.getText().toString();
+            }
+        });
+
         return rootView;
     }
 }
